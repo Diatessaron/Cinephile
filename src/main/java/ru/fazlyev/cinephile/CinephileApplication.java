22472d2056ceb16a77a85ca8c7cd7fd0a30a6542
Azat Fazlyev <afazlyev436@gmail.com>
2021-04-24 22:47:51 +0300

    Initial commit

---
+++ src/main/java/ru/fazlyev/cinephile/CinephileApplication.java
@@ -0,0 +1,11 @@
+package ru.fazlyev.cinephile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CinephileApplication.class, args);
+    }
+}
